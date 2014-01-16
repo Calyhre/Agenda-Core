2206cccab2d1e780dc6c381937faa09cc92ec787
Calyhre <calyhre@gmail.com>
2014-01-16 16:47:24 +0100

    Validate event types

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,11 @@
 class Event < ActiveRecord::Base
+  EVENT_TYPES = ['meeting', 'task', 'appointment', 'day', 'period']
+
   belongs_to :user
   has_and_belongs_to_many   :members, class_name: User
 
   default_scope { where('deleted_at IS NULL') }
 
   validates :name, :description, :start_at, :end_at, :event_type, presence: true
+  validates :event_type, inclusion: { in: EVENT_TYPES }
 end
